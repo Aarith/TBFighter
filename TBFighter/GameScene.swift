66975bfbf8e95dedb7d8bc274589726caafc8f08
Ian Campbell Brothers <iab15a@dmc-207-17.local>
2019-04-22 08:56:18 -0500

    Put placeholders in for the position of the player and enemy. Got started on creating the menu, am currently trying to create a button that prints "attack" to the console when clicked.

--- TBFighter/GameScene.swift
+++ TBFighter/GameScene.swift
@@ -14,6 +14,12 @@ class GameScene: SKScene {
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
+    let player = SKSpriteNode(color: SKColor.blue, size: CGSize(width: 150, height: 400))
+    let enemy = SKSpriteNode(color: SKColor.red, size: CGSize(width: 150, height: 400))
+    let menuback = SKSpriteNode(color: SKColor.gray, size: CGSize(width: 800, height: 600))
+    let attackbutton = SKSpriteNode(color: SKColor.red, size: CGSize(width: 180, height: 80))
+    let attackbuttontext = SKLabelNode(fontNamed: "Arial")
+    
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
@@ -29,63 +35,64 @@ class GameScene: SKScene {
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+       
+    }
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+        player.position = CGPoint(x: frame.size.width * 0.2, y: frame.size.height * 0.08)
+        player.zPosition = 10
+        addChild(player)
+        enemy.position = CGPoint(x: frame.size.width * -0.2, y: frame.size.height * 0.08)
+        enemy.zPosition = 10
+        addChild(enemy)
+        menuback.position = CGPoint(x: frame.size.width * 0, y: frame.size.height * -0.35)
+        menuback.zPosition = 0
+        addChild(menuback)
+        attackbutton.position = CGPoint(x: frame.size.width * 0, y: frame.size.height * -0.17)
+        attackbuttontext.position = CGPoint(x: frame.size.width * 0, y: frame.size.height * -0.18)
+        attackbutton.zPosition = 1
+        attackbuttontext.zPosition = 2
+        attackbuttontext.text = "Attack"
+        attackbuttontext.fontSize = 54
+        attackbuttontext.fontColor = SKColor.black
+        attackbutton.isUserInteractionEnabled = true
+        addChild(attackbutton)
+        addChild(attackbuttontext)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+        if attackbutton.contains(self) {
+            print("Attack")
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
     }
     
     
